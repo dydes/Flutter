737557a67356744dcb62493ba2f952d82d4be2b3
丁毅 <942238839@qq.com>
2022-01-06 11:21:27 +0800

    get/post

--- lib/pages/Http.dart
+++ lib/pages/Http.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 class HttpPage extends StatefulWidget {
   HttpPage({Key? key}) : super(key: key);
@@ -10,24 +10,41 @@ class HttpPage extends StatefulWidget {
 class _HttpPageState extends State<HttpPage> {
   List _list=[];
 
-  void initState(){
-    super.initState();
-    this.getData1();
+  void getHttp() async {//需要填写请求地址，headers，data
+    var dio = Dio();
+    dio.interceptors.add(LogInterceptor(responseBody: true));
+    dio.options.baseUrl = 'https://emp.willclass.com/api/v100/oms/orderInfo/page';
+    dio.options.headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "User-Agent": "HT100Billion",
+      "HT-app": 6,
+      "HT-Platform-Type":"android",
+      "HT-a":"HUQD4RRBySZrlprhZhggbwy5qLfeBsqZftDsfjHb7YK3flEvfvn26UI/d6nSWgLVatxPl2T2yVcIH3crLtDuGlK6wjncA4sP|x6zHjM2Vyag=",
+      "Cookie": " _r=ihJwwY0cYz5rlprhZhggbwy5qLfeBsqZftDsfjHb7YK3flEvfvn26au8c1HnNa22|LgRELsPpx0g=; _a=HUQD4RRBySZrlprhZhggbwy5qLfeBsqZftDsfjHb7YK3flEvfvn26UI/d6nSWgLVatxPl2T2yVcIH3crLtDuGlK6wjncA4sP|x6zHjM2Vyag=",
+    };
+    var response = await dio.post(
+      '',
+      data: {
+        "operatorId": 50000000,
+        "operatorName": "丁毅",
+        "uniteType": 8
+      },
+      options: Options(
+        contentType: Headers.jsonContentType,
+      ),
+    );
+    Map<String, dynamic> resCall = response.data;
+    print(resCall['data']['records']);
+
+setState(() {//请求返回后，刷新list数据
+  _list=resCall['data']['records'];
+});
   }
 
-  getData1() async{
-    var apiUrl = Uri.parse('https://jd.itying.com/api/pcate');
-    var response = await http.get(apiUrl);
-    if(response.statusCode==200){
-      print(json.decode(response.body));
-    }else{
-      print(response.statusCode);
-    }
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-    setState(() {
-      this._list=json.decode(response.body)["result"];
-    });
+  @override
+  initState(){
+    getHttp();//调用
   }
 
   @override
@@ -40,9 +57,9 @@ class _HttpPageState extends State<HttpPage> {
       body: this._list.length>0?ListView.builder(
         itemCount: this._list.length,
         itemBuilder:( context,index){
-          return ListTile(title: Text(this._list[index]["title"]),);
+          return ListTile(title: Text(this._list[index]['schoolName']),);//展示
         },
-      ):Text(""),
+      ):Text("jjj"),
     );
   }
   }
